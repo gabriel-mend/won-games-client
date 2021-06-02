7f987e39b095610399ff9a3fa7ae2f3e9c85a1dc
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-06-02 00:53:37 -0300

    Criando estilos para o Menu Full

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,27 +1,48 @@
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from 'components/Logo'
 import * as S from './styles'
+import { useState } from 'react'
+import Button from 'components/Button'
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+
   return (
     <S.Wrapper>
-      <S.IconWrapper>
-        <MenuIcon />
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open menu" />
       </S.IconWrapper>
-      <S.LogoWrapper>
+      <S.LogoWrapper aria-label="Logo">
         <Logo hideOnMobile />
       </S.LogoWrapper>
       <S.MenuGroup>
         <S.IconWrapper>
-          <SearchIcon />
+          <SearchIcon aria-label="Search"/>
         </S.IconWrapper>
         <S.IconWrapper>
-          <ShoppingCartIcon />
+          <ShoppingCartIcon aria-label="shopping cart"/>
         </S.IconWrapper>
       </S.MenuGroup>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)}/>
+        <S.MenuNav>
+          <S.MenuLink href="">Home</S.MenuLink>
+          <S.MenuLink href="">Explore</S.MenuLink>
+
+          <S.RegisterBox>
+            <Button fullWidth size="large">Log in now</Button>
+            <span>or</span>
+            <S.CreateAccount href="" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        </S.MenuNav>
+      </S.MenuFull>
     </S.Wrapper>
   )
 }
